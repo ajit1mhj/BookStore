0e05fcdecbfe7a0d7561189190ffc990f11acb9d
Ajit <Asus@LAPTOP-PSTR9V21>
2026-01-03 21:40:51 +0545

    Reapply "Genre Controller"
    This reverts commit 8744d9507100fd7df5e5168d86319c2a57b78716.

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddTransient<IHomeRepository, HomeRepository>();
 builder.Services.AddTransient<ICartRepository, CartRepository>();
 builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
 builder.Services.AddTransient<IStockRepository, StockRepository>();
+builder.Services.AddTransient<IGenreRepository, GenreRepositry>();
 var app = builder.Build();
 
 using(var scope = app.Services.CreateScope())
---
+++ BookStore/Repositories/GenreRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task AddGenre(Genre genre);
+        Task  UpdateGenre(Genre genre);
+        Task  DeleteGenre(Genre genre);
+        Task<Genre?> GetGenreById(int Id);
+        Task<IEnumerable<Genre>> GetAllGenres();
+    }
+
+    public class GenreRepositry : IGenreRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenreRepositry(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddGenre(Genre genre)
+        {
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateGenre(Genre genre)
+        {
+            _context.Genres.Update(genre);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteGenre(Genre genre)
+        {
+            _context.Genres.Remove(genre);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Genre?> GetGenreById(int Id)
+        {
+            return await _context.Genres.FindAsync(Id);
+        }
+        public async Task<IEnumerable<Genre>> GetAllGenres()
+        {
+            return await _context.Genres.ToListAsync();
+        }
+    }
+}
