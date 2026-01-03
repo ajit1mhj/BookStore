8744d9507100fd7df5e5168d86319c2a57b78716
Ajit <Asus@LAPTOP-PSTR9V21>
2026-01-03 21:40:16 +0545

    Revert "Genre Controller"
    This reverts commit 3a59b1289aeb421b2aca60806329572794abea79.

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddTransient<IHomeRepository, HomeRepository>();
 builder.Services.AddTransient<ICartRepository, CartRepository>();
 builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
 builder.Services.AddTransient<IStockRepository, StockRepository>();
-builder.Services.AddTransient<IGenreRepository, GenreRepositry>();
 var app = builder.Build();
 
 using(var scope = app.Services.CreateScope())
